45f0a2375f5f060fddb97bfef788aed72db74942
ronn29 <119687153+ronn29@users.noreply.github.com>
2024-12-22 22:25:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 function moveRandomElement(x) {
     x.style.position = "absolute";
-    x.style.top = Math.floor(Math.random() * 90 + 5) + "%";
-    x.style.left = Math.floor(Math.random() * 90 + 5) + "%";
+    x.style.top = Math.floor(Math.random() * 90 + 5) + "%";  
+    x.style.left = Math.floor(Math.random() * 90 + 5) + "%"; 
 }
+
 
 const moveRandom = document.querySelector("#move-random");
 
-moveRandom.addEventListener("mouseenter", function(e){
-    moveRandomElement(e.target);  // Call the function with the target element
-});
+
+function onMove(e) {
+    moveRandomElement(e.target); 
+}
+
+
+moveRandom.addEventListener("mouseenter", onMove);   
+moveRandom.addEventListener("touchstart", onMove);  
